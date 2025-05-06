5ee4581ed99c8cd0eb2d60c5bbe6d83b26e6baba
AdemCan <ademfcan@gmail.com>
2025-05-05 19:27:40 -0700

    model configs added

---
+++ src/Constants/ModelConfigs.py
@@ -0,0 +1,75 @@
+from Alt.ObjectLocalization.Inference.ModelConfig import ModelConfig
+from Alt.ObjectLocalization.Constants.InferenceC import Backend, YoloType
+
+from .Objects import ALGAE, CORAL, ROBOT
+
+# TODO move to 2024 or just delete
+# ONNX2024 = ModelConfig(
+#     "assets/yolov5s_fp32.onnx",
+#     (Object.ROBOT, Object.NOTE),
+#     2024,
+#     Backend.ONNX,
+#     YoloType.V5,
+#     ModelType.NORO,
+# )
+# RKNN2024FP32 = (
+#     "assets/yolov5s_fp32.rknn",
+#     "yolov5s-rknn-fp32",
+#     (Object.ROBOT, Object.NOTE),
+#     2024,
+#     Backend.RKNN,
+#     YoloType.V5,
+#     ModelType.NORO,
+# )
+
+ONNXSMALL2025 = ModelConfig(
+    "assets/yolov11s_fp32.onnx",
+    (ALGAE, CORAL),
+    Backend.ONNX,
+    YoloType.V11,
+)
+ONNXMEDIUM2025 = ModelConfig(
+    "assets/yolov11m_fp32.onnx",
+    (ALGAE, CORAL),
+    Backend.ONNX,
+    YoloType.V11,
+)
+
+RKNN2025INT8 = ModelConfig(
+    "assets/yolov11s_int8.rknn",
+    (ALGAE, CORAL),
+    Backend.RKNN,
+    YoloType.V11,
+)
+
+ULTRALYTICSSMALL2025 = ModelConfig(
+    "assets/yolov11s_fp32.pt",
+    (ALGAE, CORAL),
+    Backend.ULTRALYTICS,
+    YoloType.V11,
+)
+ULTRALYTICSMED2025 = ModelConfig(
+    "assets/yolov11m_fp32.pt",
+    (ALGAE, CORAL),
+    Backend.ULTRALYTICS,
+    YoloType.V11,
+)
+ALCOROULTRALYTICSSMALL2025BAD = ModelConfig(
+    "assets/yolov8s_fp32_BADDD.pt",
+    (ALGAE, CORAL, ROBOT),
+    2025,
+    Backend.ULTRALYTICS,
+    YoloType.V8,
+)
+ALCOROBEST2025GPUONLY = ModelConfig(
+    "assets/yolo11sBestTensorRT.engine",
+    (ALGAE, CORAL, ROBOT),
+    Backend.ULTRALYTICS,
+    YoloType.V11,
+)
+ALCOROBEST2025 = ModelConfig(
+    "assets/yolo11sBest_fp32.pt",
+    (ALGAE, CORAL, ROBOT),
+    Backend.ULTRALYTICS,
+    YoloType.V11,
+)
